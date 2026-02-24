d90a26f199da2258d5d08ee118351e579f473d31
vi9098 <vivekanand9098@gmail.com>
2026-02-24 17:55:55 +0530

    Implement solutions for programming questions in main.py
    Added solutions for multiple programming questions including factorial computation, dictionary generation, and input handling.

---
+++ main.py
@@ -0,0 +1,58 @@
+Question:
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 2000 and 3200 (both included).
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+Solution-:
+for i in range(2000,3200):
+    if i%7==0 and i%5!=0:
+       print(i,end=",")
+Question 2
+Level 1
+
+Question:
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+b=int(input("enter the no which u want to fac"))
+c=1
+for i in range(1,b+1):
+  c=c*i
+print(c)
+#----------------------------------------#
+Question 3
+Level 1
+
+Question:
+With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+Solution-:
+d1={}
+x=int(input("enter the value for the integral"))
+for i in range(1,x+1):
+  d1[i]=i*i
+print(d1)
+#----------------------------------------#
+Question 4
+Level 1
+
+Question:
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+Solution-:
+x=[]
+for i in range(1,10):
+  d=int(input("enter the elements of sequence-:"))
+  x.append(d)
+print(x)
+print(tuple(x))
+  
