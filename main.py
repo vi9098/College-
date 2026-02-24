7566c42785c57d6ec64765c130e8886a33321582
vi9098 <vivekanand9098@gmail.com>
2026-02-24 18:48:57 +0530

    Implement InputOutString class with string methods
    Added InputOutString class with methods for input and output.

--- main.py
+++ main.py
@@ -56,3 +56,30 @@ for i in range(1,10):
 print(x)
 print(tuple(x))
   
+#----------------------------------------#
+Question 5
+Level 1
+
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+Hints:
+Use __init__ method to construct some parameters
+
+Solution:
+class InputOutString(object):
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = raw_input()
+
+    def printString(self):
+        print self.s.upper()
+
+strObj = InputOutString()
+strObj.getString()
+strObj.printString()
+#-------------------
